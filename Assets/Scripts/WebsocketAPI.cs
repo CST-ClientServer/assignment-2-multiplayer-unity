8c231e3d59e08f2c223c51daf4a0747e48aacf57
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-01-29 05:42:57 -0800

    create class to handle websocket variant of networkAPI

---
+++ Assets/Scripts/WebsocketAPI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class WebsocketAPI : INetwork
+{
+	// TODO: Implement these
+	public void SendMessage(ByteTag tag, Vector3 data)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void SendMessage(ByteTag tag, bool data)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void SendMessage(ByteTag tag, float data)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void ReceiveMessage(NetworkController networkController)
+	{
+		throw new System.NotImplementedException();
+	}
+}
