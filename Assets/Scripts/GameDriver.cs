8b13db916ab7d2c5bc523aaa6a8ce522ded3d374
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-01-28 20:04:11 -0800

    implement start of game driver

--- Assets/Scripts/GameDriver.cs
+++ Assets/Scripts/GameDriver.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class GameDriver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Components
+	private InputManager inputManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Running variables
+	public bool IsPlaying { get; private set; } = false;
+	private static readonly float timeLimitSeconds = 60;
+	private static readonly float startDelaySeconds = 5;
+	private float timer = 0;
+	private float currentLimit;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		currentLimit = startDelaySeconds;
+		inputManager = InputManager.Instance;
+		inputManager.Disabled = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		RunPregame();
+		RunGame();
+		RunTimer();
+	}
+
+	public float GetTime()
+	{
+		return timer;
+	}
+
+	private void RunPregame()
+	{
+		if (IsPlaying) return;
+		Debug.Log("Game Not Started Yet");
+	}
+
+	private void RunGame()
+	{
+		if (!IsPlaying) return;
+
+		Debug.Log("Game Playing");
+	}
+
+	private void RunTimer()
+	{
+		if (timer > currentLimit)
+		{
+			currentLimit = currentLimit == startDelaySeconds ? timeLimitSeconds : startDelaySeconds;
+			IsPlaying = currentLimit == timeLimitSeconds;
+			inputManager.Disabled = !IsPlaying;
+			timer = 0;
+		}
+		else timer += Time.deltaTime;
+	}
 }
