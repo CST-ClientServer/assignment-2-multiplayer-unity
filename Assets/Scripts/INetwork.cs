db62e5910cf56bac2b79a799911b0aea773b037e
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-01-29 05:36:46 -0800

    create interface for multicast/websockets to implement

---
+++ Assets/Scripts/INetwork.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public interface INetwork
+{
+    public void SendMessage(ByteTag tag, Vector3 data);
+    public void SendMessage(ByteTag tag, bool data);
+    public void SendMessage(ByteTag tag, float data);
+    // Expected to be something to start on a separate thread
+    public void ReceiveMessage(NetworkController networkController);
+}
