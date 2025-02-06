90d0d6aed41b81c7e8e21612b1928cdf4338039a
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-02-06 11:27:38 -0800

    add extra line to extend TTL of UDP packet

--- Assets/Scripts/MulticastAPI.cs
+++ Assets/Scripts/MulticastAPI.cs
@@ -134,6 +134,7 @@ public class MulticastAPI : MonoBehaviour, INetwork
 			multicastSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
 			multicastSocket.Bind(localEndpoint);
 			multicastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastLoopback, false); // Disable loopback
+			multicastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 16);
 			MulticastOption options = new(IPAddress.Parse(ipAddress), IPAddress.Any);
 			multicastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, options);
 			return multicastSocket;
